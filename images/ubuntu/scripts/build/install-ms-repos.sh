531bd1289eaec66325f5376727aaafeb194371ab
Kris Stadler <21295730+KrisStadler@users.noreply.github.com>
2024-08-16 15:25:51 +0200

    accept udates

--- images/ubuntu/scripts/build/install-ms-repos.sh
+++ images/ubuntu/scripts/build/install-ms-repos.sh
@@ -12,5 +12,5 @@ dpkg -i packages-microsoft-prod.deb
 
 # update
 apt-get install apt-transport-https ca-certificates curl software-properties-common
-apt-get update
+apt-get update -y
 apt-get dist-upgrade
